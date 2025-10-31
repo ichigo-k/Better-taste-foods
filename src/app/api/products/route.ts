3b0f01672b9843395591347d1343bae2333dc6cf
tette <tettehsaintluke@gmail.com>
2025-10-31 00:03:07 +0000

    New changes

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -3,7 +3,7 @@ import prisma from "@/lib/PrismaClient";
 import { supabase } from "@/lib/supabaseClient";
 import { v4 as uuidv4 } from "uuid";
 
-export const runtime = "nodejs";
+
 
 /**
  * GET /api/products
