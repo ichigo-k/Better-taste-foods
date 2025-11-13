1499e7c7bcc2980e50042425ce4cd354dbdbd1e0
tette <tettehsaintluke@gmail.com>
2025-11-13 22:07:33 +0000

    changes

--- src/app/sections/ProductsDisplay.tsx
+++ src/app/sections/ProductsDisplay.tsx
@@ -51,7 +51,7 @@ export default function ProductsDisplay({ products }: ProductsDisplayProps) {
                                 src={item.image}
                                 alt={item.name}
                                 fill
-                                className=" group-hover:scale-105 transition-transform duration-700 rounded-lg"
+                                className="object-contain group-hover:scale-105 transition-transform duration-700 rounded-lg"
                                 priority
                             />
                             {item.outOfStock && (
